0cc533e34c47736059677410a2b872bfc8a180de
Ionut Milas <milasionut@gmail.com>
2017-11-25 18:58:24 +0200

    Adds Firebase to project

--- awesome-micro-1/src/components/Home.js
+++ awesome-micro-1/src/components/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {firebaseConnect, isLoaded, isEmpty, populate} from 'react-redux-firebase'
+import {connect} from "react-redux";
+import {compose} from "redux";
 
 const Home = () => (
     <h1>Homepage</h1>
 );
 
-export default Home;
+const populates = [
+    { child: 'owner', root: 'users' },
+    // or if you want a param of the populate child such as user's display name
+    // { child: 'owner', root: 'users', childParam: 'displayName' }
+];
+
+export default compose(
+    // gather projects and matching owners from firebase and place into redux
+    firebaseConnect([
+        { path: 'users', populates },
+    ]),
+    // projects with owner populated from redux into component props
+    connect(
+        ({ firebase }) => ({
+            projects: populate(firebase, 'users', populates),
+        })
+    )
+)(Home)
--- awesome-micro-1/src/redux/store.js
+++ awesome-micro-1/src/redux/store.js
@@ -2,10 +2,21 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { reducer as reduxFormReducer } from "redux-form";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
+import { reactReduxFirebase, getFirebase, firebaseStateReducer } from 'react-redux-firebase';
+
 import * as reducers from "./ducks";
 import { loadState, saveState } from "./localStorage";
 import Helpers from "../support/helpers";
 import api from "./middlewares/api";
+
+const fbConfig = {
+    apiKey: "AIzaSyCuJ6Q4R_gki046rem94y8Mb4T_jO4ZlX4",
+    authDomain: "gleaming-idiom-167311.firebaseapp.com",
+    databaseURL: "https://gleaming-idiom-167311.firebaseio.com",
+    projectId: "gleaming-idiom-167311",
+    storageBucket: "gleaming-idiom-167311.appspot.com",
+    messagingSenderId: "138059995471"
+};
 
 export default function configureStore( ) {
     const enhancers = [];
@@ -14,13 +25,14 @@ export default function configureStore( ) {
 
     const rootReducer = combineReducers( {
         ...reducers,
+        firebase: firebaseStateReducer,
         form: reduxFormReducer
     } );
 
     const middleware = [
         api,
         loggerMiddleware,
-        thunk
+        thunk.withExtraArgument(getFirebase)
     ];
 
     if ( process.env.NODE_ENV === "development" ) {
@@ -33,6 +45,7 @@ export default function configureStore( ) {
 
     const composedEnhancers = compose(
         applyMiddleware( ...middleware ),
+        reactReduxFirebase(fbConfig, { userProfile: 'users', enableLogging: false }),
         ...enhancers,
     );
 
